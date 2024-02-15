5c7e4cb2b7f28c59e0bbe936aa12fa916466d694
Nguyễn Quang Huy <95010802+HuyCan1605@users.noreply.github.com>
2024-02-15 13:15:07 +0700

    Update watch_folder.sh

--- Code and algorithm/BASH/watch_folder.sh
+++ Code and algorithm/BASH/watch_folder.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# định nghĩa biến PYTHON_SCRIPT và  WATCH_FOLDER
+# Difine variables PYTHON_SCRIPT and  WATCH_FOLDER
 PYTHON_SCRIPT="/home/pi/Documents/Detect_plate/main.py"
 PYTHON_SCRIPT2="/home/pi/Documents/Detect_plate/main2.py"
 WATCH_FOLDER="/var/www/html/uploads"
 
 while true; do
-    # Sử dụng inotifywait để theo dõi thau đổi trong thư mục
+    # Using inotifywait to watching the change in folder
     inotifywait -e close_write -q "$WATCH_FOLDER"
     
-    # Khi có sự kiện thay đổi, kích hoạt script Python
+    # When an event occur, activate script
     /home/pi/Documents/Detect_plate/myenv/bin/python3 "$PYTHON_SCRIPT"
     #/home/pi/Documents/Detect_plate/myenv/bin/python3 "$PYTHON_SCRIPT2"
     sleep 1
